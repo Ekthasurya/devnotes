8a887344be934c8c8c93e8f8b11ddecca3ab28b7
Ekthasurya <suryakiranmajhi1999@gmail.com>
2024-08-25 14:41:50 +0530

    my whole notes app backend

---
+++ model/user.model.js
@@ -0,0 +1,17 @@
+import mongoose  from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
+    gender:{type:String,enum:["male","female"],required:true},
+    age:{type:String,required:true}
+},{
+    versionKey:false,
+    timestamps:true
+    
+})
+
+const UserModel = mongoose.model("User",userSchema);
+
+export { UserModel };
